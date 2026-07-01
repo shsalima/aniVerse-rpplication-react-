ea026c861fe7a1ff1bfd921b41b2f77d7a765bb5
salima <sahisalima77@gmail.com>
2026-07-01 16:04:57 +0200

    travail sur section de cherche et filtragncode jsx et style

--- src/pages/Anime.jsx
+++ src/pages/Anime.jsx
@@ -1,9 +1,11 @@
 import AnimeHeader from "../components/Anime/AnimeHeader";
+import SearchFilter from "../components/Anime/SearchFilter";
 
 export default function Anime() {
     return (    
         <div className="container mx-auto px-4">
             <AnimeHeader/>
+            <SearchFilter/>
 
         </div>
     )
